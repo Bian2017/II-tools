20d5eb3d65c197d22c4f1ae1ccb67604a413adcd
Bian2017 <bgl_cumt_zju@163.com>
2020-03-20 11:50:46 +0800

    feat:搭建简易运行环境

---
+++ bin/ii-tools-run.js
@@ -0,0 +1,2 @@
+#! /usr/bin/env node
+require('../lib/cli/run'); // 需在文件最开头写上 #!/usr/bin/env node，否则文件里的脚本不会再Node环境下执行
---
+++ bin/ii-tools.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+require('../lib/cli/'); // 需在文件最开头写上 #!/usr/bin/env node，否则文件里的脚本不会再Node环境下执行
